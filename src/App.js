cf9f6444b4dfaa8836b8d1e7c18ad43987d86951
ftcjunior <126026798+ftcjunior@users.noreply.github.com>
2023-04-15 08:47:06 -0300

    TESTE AULAS DE REACT

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Calcular from './components/Testecomponents'
+import Calc3 from './Component3'
 function App() {
+
+  let nome = 'francisco targino chagas junior'
+  let idade = 52
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p> nome:{nome}</p>
+      <p>idade: {idade}</p>
+     <Calcular/>
+     <Calc3/>
+     
     </div>
   );
 }
